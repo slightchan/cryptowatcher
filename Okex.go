ab2d60d95c3b94af432d9950a32ed4bf3069001c
Logan <slightchan@hotmail.com>
2018-02-28 16:16:10 +0800

    2
    2

--- Okex.go
+++ Okex.go
@@ -5,9 +5,7 @@
 package main
 
 import (
-	"flag"
 	"log"
-	"net/url"
 	"os"
 	"os/signal"
 	"time"
@@ -15,23 +13,20 @@ import (
 	"github.com/gorilla/websocket"
 )
 
-var addr = flag.String("addr", "real.okex.com:10441", "http service address")
-
 const (
 	WS_API_URL_OKCOIN = "wss://real.okcoin.com:10440/websocket/okcoinapi"
 	WS_API_URL_OKEX   = "wss://real.okex.com:10441/websocket"
 )
 
+var gDone = make(chan int)
+
 func connectOkex() {
-	flag.Parse()
 	log.SetFlags(0)
-	log.Println(websocket.DefaultDialer)
 
 	interrupt := make(chan os.Signal, 1)
+	kill := make(chan os.Signal, 1)
 	signal.Notify(interrupt, os.Interrupt)
-
-	u := url.URL{Scheme: "wss", Host: *addr, Path: "/websocket"}
-	log.Printf("connecting to %s", u.String())
+	signal.Notify(kill, os.Kill)
 	var dialer = websocket.Dialer{ReadBufferSize: 10000, WriteBufferSize: 1000}
 	dialer.EnableCompression = true
 	//dialer.Proxy = nil
@@ -41,39 +36,32 @@ func connectOkex() {
 	if err != nil {
 		log.Fatal("dial:", err)
 	}
-	log.Println("Dial OK")
+	log.Println("Dial ok")
 	defer c.Close()
 
-	done := make(chan struct{})
 	c.WriteMessage(websocket.TextMessage, []byte("{'event':'ping'}"))
-	ticker := time.NewTicker(time.Second)
-	go func() {
-		defer close(done)
-		for {
-			_, message, err := c.ReadMessage()
-			if err != nil {
-				log.Println("read:", err)
-				return
-			}
-			log.Printf("recv: %s", message)
-		}
-	}()
-
+	ticker := time.NewTicker(time.Second * 30)
 	defer ticker.Stop()
+	go onRecv(c)
 
 	for {
 		select {
-		case <-done:
-			return
-		case t := <-ticker.C:
-			//err := c.WriteMessage(websocket.TextMessage, []byte(t.String()))
-			//if err != nil {
-			//log.Println("write:", err)
-			//return
-			//}
+		case <-ticker.C:
 			c.WriteMessage(websocket.TextMessage, []byte("{'event':'ping'}"))
-			log.Println(t)
-			//			return
+		case <-kill:
+			log.Println("kill")
+
+			// Cleanly close the connection by sending a close message and then
+			// waiting (with timeout) for the server to close the connection.
+			err := c.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+			if err != nil {
+				log.Println("write close:", err)
+				return
+			}
+			select {
+			case <-time.After(time.Second):
+			}
+			return
 		case <-interrupt:
 			log.Println("interrupt")
 
@@ -85,10 +73,23 @@ func connectOkex() {
 				return
 			}
 			select {
-			case <-done:
 			case <-time.After(time.Second):
 			}
 			return
 		}
+	}
+}
+
+func onRecv(c *websocket.Conn) {
+	for {
+		_, message, err := c.ReadMessage()
+		if err != nil {
+			log.Println("read:", err)
+			gDone <- 1
+			return
+		}
+		log.Printf("recv: %s", message)
+		<-time.After(time.Second * 5)
+		gDone <- 1
 	}
 }
--- main.go
+++ main.go
@@ -12,4 +12,6 @@ func main() {
 	fmt.Println(gPriceData["USD/JPY"])
 	fmt.Println(gPriceData["USD/CNY"])
 	fmt.Printf("CNY/JPY,%.4f\n", gPriceData["USD/CNY"].Price/gPriceData["USD/JPY"].Price)
+	go connectOkex()
+	<-gDone
 }
